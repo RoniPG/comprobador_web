3d376ec21190e943068746a8878e5714cb2fda99
RoniPG <ronigmn@gmail.com>
2026-01-19 18:16:59 +0100

    feat: create initial Tkinter window structure

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+def main():
+    ventana = tk.Tk()
+    ventana.title("Comprobador de Conectividad Web")
+    ventana.geometry("400x200")
+    ventana.resizable(False, False)
+
+    ventana.mainloop()
+
+if __name__ == "__main__":
+    main()
