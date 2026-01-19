ac56afca3b5ae787257bb26a185d10ee7afa933f
RoniPG <ronigmn@gmail.com>
2026-01-19 18:35:29 +0100

    feat: add button and HTTP connectivity check logic

--- src/main.py
+++ src/main.py
@@ -1,6 +1,22 @@
 import tkinter as tk
+from urllib.request import urlopen
+
+
+def comprobar_web():
+    url = entrada_url.get()
+    try:
+        response = urlopen(url=url)
+        if response.getcode() == 200:
+            etiqueta_resultado.config(text=" La web está disponible")
+        else:
+            etiqueta_resultado.config(text=" La web no está disponible")
+    except:
+        etiqueta_resultado.config(text=" Error al conectar")
+
 
 def main():
+    global entrada_url, etiqueta_resultado
+
     ventana = tk.Tk()
     ventana.title("Comprobador de Conectividad Web")
     ventana.geometry("400x200")
@@ -12,10 +28,13 @@ def main():
     entrada_url = tk.Entry(ventana, width=50)
     entrada_url.pack()
 
+    tk.Button(ventana, text="Comprobar", command=comprobar_web).pack(pady=10)
+
     etiqueta_resultado = tk.Label(ventana, text="")
     etiqueta_resultado.pack(pady=15)
 
     ventana.mainloop()
+
 
 if __name__ == "__main__":
     main()
